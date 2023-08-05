0c558cfb1ca35cb9a4bc85e90fa09974b2a03202
Giorgi Londaridze <londara16@gmail.com>
2023-08-05 21:45:59 +0400

    style: added hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import "./styles.scss";
 import HeaderMenu from "./components/HeaderMenu";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 // import Stories from "./components/Stories";
 // import Features from "./components/Features";
-import Pricing from "./components/Pricing";
+// import Pricing from "./components/Pricing";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <HeaderMenu />
-      {/* <Home /> */}
+      <Home />
       {/* <Stories /> */}
       {/* <Features /> */}
-      <Pricing />
+      {/* <Pricing /> */}
 
       <Footer />
     </>
--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import Logo from "../assets/shared/desktop/logo.svg";
 import Menu from "../assets/shared/mobile/menu.svg";
+import X from "../assets/shared/mobile/close.svg";
 
-const HeaderMenu: React.FC = () => (
-  <>
-    <div className="header">
-      <img src={Logo} />
-      <img src={Menu} alt="" />
-    </div>
-  </>
-);
+const HeaderMenu = () => {
+  const [menu, openMenu] = useState(false);
+  console.log(menu);
+
+  return (
+    <>
+      <div className="header">
+        <img src={Logo} />
+        <img src={menu ? X : Menu} onClick={() => openMenu(!menu)} />
+      </div>
+
+      <div className={menu ? "menu" : "menuNone"}>
+        <button className="storiesLink">STORIES</button>
+        <button className="featuresLink">FEATURES</button>
+        <button className="pricingLink">PRICING</button>
+
+        <div className="line"></div>
+
+        <button className="inviteButton">GET AN INVITE</button>
+      </div>
+
+      <div className={menu ? "blur" : ""}></div>
+    </>
+  );
+};
 
 export default HeaderMenu;
